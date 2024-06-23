6798b85abe943841f90b7599bc6cd567b27c6312
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-23 19:13:01 +0500

    comment js line 66
    continue

--- app.js
+++ app.js
@@ -1,63 +1,67 @@
-const mobileBrands = [
+let mobileBrands = [
     {
-      brand: "Apple",
-      model: "iPhone 13",
-      color: "Midnight"
+      "brand": "Apple",
+      "model": "iPhone 13",
+      "color": "Midnight"
     },
     {
-      brand: "Samsung",
-      model: "Galaxy S21",
-      color: "Phantom Gray"
+      "brand": "Samsung",
+      "model": "Galaxy S21",
+      "color": "Phantom Gray"
     },
     {
-      brand: "Google",
-      model: "Pixel 6",
-      color: "Stormy Black"
+      "brand": "Google",
+      "model": "Pixel 6",
+      "color": "Stormy Black"
     },
     {
-      brand: "OnePlus",
-      model: "OnePlus 9",
-      color: "Winter Mist"
+      "brand": "OnePlus",
+      "model": "OnePlus 9",
+      "color": "Winter Mist"
     },
     {
-      brand: "Xiaomi",
-      model: "Mi 11",
-      color: "Horizon Blue"
+      "brand": "Xiaomi",
+      "model": "Mi 11",
+      "color": "Horizon Blue"
     },
     {
-      brand: "Oppo",
-      model: "Find X3 Pro",
-      color: "Gloss Black"
+      "brand": "Oppo",
+      "model": "Find X3 Pro",
+      "color": "Gloss Black"
     },
     {
-      brand: "Vivo",
-      model: "X60 Pro",
-      color: "Midnight Black"
+      "brand": "Vivo",
+      "model": "X60 Pro",
+      "color": "Midnight Black"
     },
     {
-      brand: "Sony",
-      model: "Xperia 5 II",
-      color: "Black"
+      "brand": "Sony",
+      "model": "Xperia 5 II",
+      "color": "Black"
     },
     {
-      brand: "LG",
-      model: "Wing",
-      color: "Illusion Sky"
+      "brand": "LG",
+      "model": "Wing",
+      "color": "Illusion Sky"
     },
     {
-      brand: "Motorola",
-      model: "Moto G100",
-      color: "Iridescent Sky"
+      "brand": "Motorola",
+      "model": "Moto G100",
+      "color": "Iridescent Sky"
     },
     {
-      brand: "Nokia",
-      model: "8.3 5G",
-      color: "Polar Night"
+      "brand": "Nokia",
+      "model": "8.3 5G",
+      "color": "Polar Night"
     },
     {
-      brand: "Huawei",
-      model: "P40 Pro",
-      color: "Silver Frost"
+      "brand": "Huawei",
+      "model": "P40 Pro",
+      "color": "Silver Frost"
     }
   ];
-  
+let main = document.querySelector("#main");
+
+for(let keys of mobileBrands){
+    // console.log(mobileBrands[keys].color)
+}
