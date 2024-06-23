e464d8db7f2fd1714aa45977001ee18669e1b621
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-23 19:01:45 +0500

    js line 62
    continue

---
+++ app.js
@@ -0,0 +1,63 @@
+const mobileBrands = [
+    {
+      brand: "Apple",
+      model: "iPhone 13",
+      color: "Midnight"
+    },
+    {
+      brand: "Samsung",
+      model: "Galaxy S21",
+      color: "Phantom Gray"
+    },
+    {
+      brand: "Google",
+      model: "Pixel 6",
+      color: "Stormy Black"
+    },
+    {
+      brand: "OnePlus",
+      model: "OnePlus 9",
+      color: "Winter Mist"
+    },
+    {
+      brand: "Xiaomi",
+      model: "Mi 11",
+      color: "Horizon Blue"
+    },
+    {
+      brand: "Oppo",
+      model: "Find X3 Pro",
+      color: "Gloss Black"
+    },
+    {
+      brand: "Vivo",
+      model: "X60 Pro",
+      color: "Midnight Black"
+    },
+    {
+      brand: "Sony",
+      model: "Xperia 5 II",
+      color: "Black"
+    },
+    {
+      brand: "LG",
+      model: "Wing",
+      color: "Illusion Sky"
+    },
+    {
+      brand: "Motorola",
+      model: "Moto G100",
+      color: "Iridescent Sky"
+    },
+    {
+      brand: "Nokia",
+      model: "8.3 5G",
+      color: "Polar Night"
+    },
+    {
+      brand: "Huawei",
+      model: "P40 Pro",
+      color: "Silver Frost"
+    }
+  ];
+  
